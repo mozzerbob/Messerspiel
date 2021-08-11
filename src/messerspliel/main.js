27f5bb211704622b0bd9c95033879cefb7e787bf
Phil Morris <morris.p1990@gmail.com>
2021-08-11 16:47:41 +0100

    Main unit tests

--- src/messerspliel/main.js
+++ src/messerspliel/main.js
@@ -1,32 +1,18 @@
-import roll from './dice'
-
-// export default function risk(a) {
-//     if (a[0] === undefined) return;
-
-//     const result = roll(6);
-//     a[0].face = result;
-//     if (result <= a.length) a.pop();
-
-//     return a
-// }
+import roll from './dice';
 
 export function rollAction(a) {
   let highest
-  console.log('rollaction')
-  console.log(a)
   if (a === undefined) return highest;
+
   let pop = false
   a.forEach(d => {
+    console.log(d)
     if (d.selected) {  
       d.face = roll(6);
       d.selected = false;
 
-      if (highest?.face === undefined) {
-        highest = {...d};
-      }
-      else if (d.face > highest?.face) {
-        highest = {...d};
-      };
+      if (highest?.face === undefined) highest = {...d};
+      else if (d.face > highest?.face) highest = {...d};
 
       if (d.risk === true) {
         if (d.face <= a.length) pop = true;
--- src/messerspliel/main.test.js
+++ src/messerspliel/main.test.js
@@ -1,4 +1,5 @@
-import { procOutcome } from './main';
+import { procOutcome, rollAction } from './main';
+import { roll } from './dice';
 
 test('want a good outcome', () => {
     expect(procOutcome(6)).toEqual('good');
@@ -11,3 +12,89 @@ test('want a mixed outcome', () => {
 test('want a bad outcome', () => {
     expect(procOutcome(3)).toEqual('bad');
 });
+
+test('resolve action selected', () => {
+    expect(rollAction(a_selected) !== undefined)
+});
+
+test('resolve action un-selected', () => {
+    expect(rollAction(a_unselected) === undefined)
+});
+
+const a_selected = [
+    {
+        "key": 0,
+        "face": 1,
+        "risk": true,
+        "selected": true
+    },
+    {
+        "key": 1,
+        "face": 4,
+        "risk": false,
+        "selected": true
+    },
+    {
+        "key": 2,
+        "face": 5,
+        "risk": false,
+        "selected": true
+    },
+    {
+        "key": 3,
+        "face": 5,
+        "risk": false,
+        "selected": true
+    },
+    {
+        "key": 4,
+        "face": 1,
+        "risk": false,
+        "selected": true
+    },
+    {
+        "key": 5,
+        "face": 1,
+        "risk": false,
+        "selected": true
+    }
+];
+
+const a_unselected = [
+    {
+        "key": 0,
+        "face": 1,
+        "risk": true,
+        "selected": false
+    },
+    {
+        "key": 1,
+        "face": 4,
+        "risk": false,
+        "selected": false
+    },
+    {
+        "key": 2,
+        "face": 5,
+        "risk": false,
+        "selected": false
+    },
+    {
+        "key": 3,
+        "face": 5,
+        "risk": false,
+        "selected": false
+    },
+    {
+        "key": 4,
+        "face": 1,
+        "risk": false,
+        "selected": false
+    },
+    {
+        "key": 5,
+        "face": 1,
+        "risk": false,
+        "selected": false
+    }
+];
